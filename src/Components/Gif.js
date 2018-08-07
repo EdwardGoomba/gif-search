a1e667e45ff49421f161136b4d946c1b747465d8
EdwardGoomba <ask@edwardvdesign.com>
2018-08-07 15:43:44 -0400

    Display gifs

--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const Gif = props => (
   <li className="gif-wrap">
-    <img alt=""/>
+    <img src={props.url} alt=""/>
   </li>
-);
+)
 
-export default Gif;
+export default Gif
